5d00c9497c63738e03918793f7748f75fc729c75
Mac <mac@Macs-MacBook-Pro.local>
2025-01-12 00:58:53 +0100

    first commit

---
+++ src/utils/data.js
@@ -0,0 +1,161 @@
+// import { HiOutlineDesktopComputer } from "react-icons/hi";
+import { CiMobile1 } from "react-icons/ci";
+import { MdWorkspacesOutline } from "react-icons/md";
+import { TbBusinessplan } from "react-icons/tb";
+import { MdOutlineBusinessCenter } from "react-icons/md";
+import { GiTrade } from "react-icons/gi";
+import bet from "../assets/bet.jpeg";
+export const projectExperience = [
+  {
+    name: "Business Development",
+    projects: 76,
+    icon: TbBusinessplan,
+    bg: "#286F6C",
+  },
+  {
+    name: "Crypto Investment",
+    projects: 63,
+    icon: GiTrade,
+    bg: "#EEC048",
+  },
+  {
+    name: "Investment Banker & Consultant",
+    projects: 47,
+    icon: MdOutlineBusinessCenter,
+    bg: "#F26440",
+  },
+];
+
+export const WhatDoIHelp = [
+  "As an Expert, I will help you in finding a solution in Business Development.",
+  "As an Expert, I will help you in finding a solution in crypto Investment and crypto currency mining.",
+  "A Consultant & Expert in Investment Banking.",
+];
+
+export const workExp = [
+  {
+    place: bet,
+    // tenure: "Aug 2014 - Sep 2016",
+    role: "Crypto Investment",
+    detail:
+      "Beatrice Charlotte Lee is a distinguished professional who has earned a certification validating their exceptional expertise in broker, cryptocurrencies, stock markets and the broader blockchain industry.",
+  },
+  {
+    // place: "New Man Services",
+    // tenure: "Aug 2014 - Sep 2016",
+    role: "Investment Banker & Consultant",
+    detail:
+      "She started investing in crypto in late 2009. The more I learnt about the technology that crypto is built on top of (blockchain), the more I began to understand what profound impact this new technology would have on the world. From my hands on experience using crypto, needing to use exchanges, setting up my own personal wallets and more.",
+  },
+  // {
+  //   place: "Global Solution",
+  //   tenure: "Aug 2014 - Sep 2016",
+  //   role: "Sr. Product Designer",
+  //   detail:
+  //     "A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+  // },
+];
+
+export const comments = [
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+];
+
+export const sliderSettings = {
+  dots: true,
+  infinite: false,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  initialSlide: 0,
+  touchMove: true,
+  useCSS: true,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
